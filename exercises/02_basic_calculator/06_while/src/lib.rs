736c49fabd8fac4447bf62c9771ab636891071da
SWARNIM <swarnimchandve23@gmail.com>
2025-04-05 20:07:58 +0000

    2nd day

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,8 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    // todo!()
+
     // The `while` loop is used to repeat a block of code
+    let mut result = 1;
+    let mut i  = n;
+    while i> 0{
+        result *=i;
+        i-=1;
+    }
+    return result;
 }
 
 #[cfg(test)]
